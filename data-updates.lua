1b3dec0bd823c0db04fb6c74ca3bf3214a7b1a9d
Sil3ntStorm <Sil3ntStorm@users.noreply.github.com>
2020-12-24 17:04:38 +0100

    0.16-0.18 by 8ZN (https://mods.factorio.com/mod/Command_and_Conquer_Low_Power_Alerts-016)

--- data-updates.lua
+++ data-updates.lua
@@ -2,8 +2,8 @@ local speaker = data.raw["programmable-speaker"]["programmable-speaker"]
 speaker.instruments[#speaker.instruments+1] = {
     name = "c&c-low-power-alerts",
     notes = {
-        {name = "Oldschool Warning", sound = {filename = "__Command_and_Conquer_Low_Power_Alerts__/sound/lowpoweroldschool.ogg"}},
-        {name = "Red Alert Warning", sound = {filename = "__Command_and_Conquer_Low_Power_Alerts__/sound/lowpowerredalert.ogg"}},
-        {name = "Tiberian Sun Warning", sound = {filename = "__Command_and_Conquer_Low_Power_Alerts__/sound/lowpowertiberiansun.ogg"}},
+        {name = "Oldschool Warning", sound = {filename = "__Command_and_Conquer_Low_Power_Alerts-016__/sound/lowpoweroldschool.ogg"}},
+        {name = "Red Alert Warning", sound = {filename = "__Command_and_Conquer_Low_Power_Alerts-016__/sound/lowpowerredalert.ogg"}},
+        {name = "Tiberian Sun Warning", sound = {filename = "__Command_and_Conquer_Low_Power_Alerts-016__/sound/lowpowertiberiansun.ogg"}},
     }
 }
